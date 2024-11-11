f04d99b58afa5212bf34527d4edeea07f65ba063
ianb <ian246@hotmail.co.uk>
2024-11-11 10:05:44 +0000

    feat: added products and category in models.py, products app

--- coffeeshop/products/models.py
+++ coffeeshop/products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+class Category(models.Model):
+    """model for products category"""
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Products(models.Model):
+    """model for products and their details"""
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    product_name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.FloatField(validators=[MinValueValidator(
+        1), MaxValueValidator(5)], null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
