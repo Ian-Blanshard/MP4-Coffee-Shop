8dc7cab4dac1e79c579695468dc6e0858ed43bcb
ianb <ian246@hotmail.co.uk>
2024-11-11 18:59:51 +0000

    feat: added and created basic structure of products.html in product app

--- coffeeshop/coffeeshop/settings.py
+++ coffeeshop/coffeeshop/settings.py
@@ -77,6 +77,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 # ensures media is accessible accross all apps
+                 'django.template.context_processors.media',
                  # `allauth` needs this from django
                 'django.template.context_processors.request',
             ],
@@ -151,11 +153,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
