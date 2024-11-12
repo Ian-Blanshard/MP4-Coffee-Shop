8af33803aac1295c2be8e7577bc48c233e597cd2
ianb <ian246@hotmail.co.uk>
2024-11-12 19:23:07 +0000

    feat: added sort dropdown in products.html

--- coffeeshop/products/views.py
+++ coffeeshop/products/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 # Create your views here.
 
 def all_products(request):
@@ -11,11 +12,31 @@ def all_products(request):
     products = Product.objects.all()
     # for loading page without query
     query = None
+    sort = None
+    direction = None
+    category = None
+    # set default sort key
+    sortkey = 'id'
+
     
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower('product_name'))
+
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+        products = products.order_by(sortkey)
+
         if 'category' in request.GET:
-            categories = request.GET['category']
-            products = products.filter(category__name=categories)
+            category = request.GET['category']
+            if Category.objects.filter(name=category).exists():
+                products = products.filter(category__name=category)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -26,9 +47,13 @@ def all_products(request):
             queries = Q(product_name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
+        'selected_category': category,
     }
 
     return render(request, 'products/products.html', context)
