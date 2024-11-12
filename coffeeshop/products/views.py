df316a85755ba406188821ecaefb4c277e34324e
ianb <ian246@hotmail.co.uk>
2024-11-12 16:40:01 +0000

    feat: added links to categories of products in full_screen_nav.html and mobile_screen_nav.html, updated all_products in views.py

--- coffeeshop/products/views.py
+++ coffeeshop/products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 def all_products(request):
@@ -11,7 +11,12 @@ def all_products(request):
     products = Product.objects.all()
     # for loading page without query
     query = None
+    
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            products = products.filter(category__name=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -20,8 +25,6 @@ def all_products(request):
             # use Q to search if query is in either name or description
             queries = Q(product_name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-
-
 
     context = {
         'products': products,
