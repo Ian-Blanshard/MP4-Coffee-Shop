ec12c42dae908a8714123fe8c670f0512f82a2e5
ianb <ian246@hotmail.co.uk>
2024-11-20 22:04:44 +0000

    feat: added profile.html in profiles app, added functionality in checkout_success to save delivery info to profile

--- coffeeshop/profiles/models.py
+++ coffeeshop/profiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
