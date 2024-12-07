1dff76f8d7cc16efa93c74a5520bd07109a187fd
ianb <ian246@hotmail.co.uk>
2024-12-07 15:26:09 +0000

    testing: python code validation/commenting for coffeeshop

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -25,20 +25,21 @@ if os.path.exists(BASE_DIR / '.env'):
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('MY_SECRET_KEY', 'your_default_secret_key')
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'your_default_stripe_public_key')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'your_default_stripe_secret_key')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'your_default_stripe_webhook_secret')
-
-
+SECRET_KEY = os.getenv('MY_SECRET_KEY',
+                       'your_default_secret_key')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY',
+                              'your_default_stripe_public_key')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY',
+                              'your_default_stripe_secret_key')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET',
+                             'your_default_stripe_webhook_secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['coffee-shop-mp4-a883163ad215.herokuapp.com','localhost','127.0.0.1']
-
-
+ALLOWED_HOSTS = [
+    'coffee-shop-mp4-a883163ad215.herokuapp.com',
+    'localhost',
+    '127.0.0.1']
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -66,10 +67,9 @@ INSTALLED_APPS = [
 
 
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #whitenoise
+    # whitenoise
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -81,9 +81,7 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 
 ]
-
 ROOT_URLCONF = 'coffeeshop.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -99,7 +97,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                 # ensures media is accessible accross all apps
+                # ensures media is accessible accross all apps
                 'django.template.context_processors.media',
                 # ensures bag is accessible accross all apps
                 'bag.contexts.bag_contents',
@@ -110,17 +108,14 @@ TEMPLATES = [
         },
     },
 ]
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
 SITE_ID = 1
-
 # temporarily send confirmation emails to console
-EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -131,11 +126,8 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'coffeeshop.wsgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
@@ -147,43 +139,36 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.\
+                password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 # crispy forms packs
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
 CRISPY_TEMPLATE_PACK = "bootstrap5"
-
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'coffee-shop-mp4'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -198,7 +183,6 @@ if 'USE_AWS' in os.environ:
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 STORAGES = {
     'default': {
         'BACKEND': 'custom_storages.MediaStorage',
@@ -209,36 +193,19 @@ STORAGES = {
         'LOCATION': 'static',
     },
 }
-
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-
-
 DEFAULT_FROM_EMAIL = 'coffeeshop@example.com'
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-
-# stripe 
-
+# stripe
 STRIPE_CURRENCY = 'gbp'
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-
