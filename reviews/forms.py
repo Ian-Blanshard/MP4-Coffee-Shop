625408905545c34d0e927ed475765a410b5eadd3
ianb <ian246@hotmail.co.uk>
2024-12-07 13:20:49 +0000

    feat: added dropdown to add review form in reviews app

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,3 +7,17 @@ class ReviewForm(forms.ModelForm):
         model = Reviews
         # fields to include in the form
         fields = ('review', 'rating')
+
+    number_of_stars = [
+        (1, '1 - Poor'),
+        (2, '2 - Fair'),
+        (3, '3 - Good'),
+        (4, '4 - Very Good'),
+        (5, '5 - Excellent'),
+    ]
+
+    rating = forms.ChoiceField(
+        choices=number_of_stars,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Number of Stars (1 to 5)',
+    )
