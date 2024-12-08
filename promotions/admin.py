ceb7bcae23846f75478ae0905e7a258d5dc4b94e
ianb <ian246@hotmail.co.uk>
2024-12-08 11:57:27 +0000

    feat: added discounts to admin page in django

--- promotions/admin.py
+++ promotions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Discount
 
-# Register your models here.
+
+class DiscountAdmin(admin.ModelAdmin):
+    fields = ('product', 'percentage', 'discounted_price')
+    readonly_fields = ('discounted_price',)
+    ordering = ('product',)
+
+    def discounted_price(self, obj):
+        return obj.discounted_price()
+    discounted_price.short_description = 'Discounted Price'
+
+
+admin.site.register(Discount, DiscountAdmin)
