1484f7b00ebba2305a62b97c4aaf9ca44f94a660
ianb <ian246@hotmail.co.uk>
2024-12-08 11:37:40 +0000

    feat: added custom error handler pages

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -33,6 +33,10 @@ urlpatterns = [
     path('promotions/', include('promotions.urls')),
 ]
 
+# Custom error handlers
+handler404 = 'core.views.custom_404' 
+handler500 = 'core.views.custom_500'
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'core/index.html')
+
+def custom_404(request, exception=None):
+    """a view for 404 errors"""
+    return render(request, '404.html', status=404)
+
+def custom_500(request):
+    """a view for 500 errors"""
+    return render(request, '500.html', status=500)
